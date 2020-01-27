c1c22048eddc44be95224ac0baabb1da336ff1c1
max-jellycat <max@jellycat.fr>
2020-01-27 13:22:49 +0100

    setup part 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
       <main className="section">
         <h1 className="title">Hello World!</h1>
         <h2 className="subtitle">What will we build today?</h2>
+        <button className="button is-primary is-outlined">
+          <span className="icon">
+            <i className="fas fa-user" />
+          </span>
+          <span>Hello</span>
+        </button>
       </main>
     </section>
   );
